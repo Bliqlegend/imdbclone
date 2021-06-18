827bacbc5e86577c6f734023db676a90309a0fca
Tanush Yadav <53972008+Bliqlegend@users.noreply.github.com>
2021-06-18 18:02:45 +0530

    Filter added

--- imdbclone2/settings.py
+++ imdbclone2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'user_app',
+    'django_filter'
 ]
 
 MIDDLEWARE = [
@@ -148,6 +149,8 @@ REST_FRAMEWORK = {
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework.authentication.BasicAuthentication',
     # ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
